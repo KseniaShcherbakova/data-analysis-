4216f6aa9508407e64395c45c3895028eaa32b05
Ksenia Shcherbakova <112802680+KseniaShcherbakova@users.noreply.github.com>
2022-12-16 17:37:18 +0500

    Update 6.1_lab.R

--- lab4 (diet)/6.1_lab.R
+++ lab4 (diet)/6.1_lab.R
@@ -13,35 +13,50 @@ summary(data)
 colnames(data) <- c("gender", "age", "height", "initial.weight", 
                     "diet.type", "final.weight")
 data$diet.type <- factor(c("A", "B", "C")[data$diet.type])
-#Диеты привелись к нормальному типу
 
-#Изменим данные о гендере
 data$gender <- as.factor(data$gender)
 par(mfrow=c(1,2)) 
 data$gender <- factor(c("male", "female")[data$gender])
 summary(data)
-#Гендеры привелись к нормальному типу
+#Диеты привелись к нормальному типу
 
 #Добавим новую колонку - Похудение
 data$weight.loss = data$initial.weight - data$final.weight
 summary(data)
+
 #Проанализиуем есть ли различия по типам диет
 boxplot(weight.loss~diet.type,data=data,col="light gray",
         ylab = "Weight loss (kg)", xlab = "Diet type")
 abline(h=0,col="green")
 
-#проверим сбалансированные ли данные
+#Добавляем проверку на зависисмость потери веса от пола 
+boxplot(weight.loss~gender,data=data,col="light gray",
+        ylab = "Weight loss (kg)", xlab = "Diet type")
+abline(h=0,col="green")
+
+#проверим сбалансированные ли данные для диеты
 table(data$diet.type)
 
-#График групповых средних
+#проверим сбалансированные ли данные для гендера
+table(data$gender)
+
+#График групповых средних для диеты
 plotmeans(weight.loss ~ diet.type, data=data)
 aggregate(data$weight.loss, by = list(data$diet.type), FUN=sd)
 
+#График групповых средних для гендера
+plotmeans(weight.loss ~ gender, data=data)
+aggregate(data$weight.loss, by = list(data$gender), FUN=sd)
 
 #Для подгонки ANOVA модели используем функцию aov, частный случай линейной модели lm
-#тест на межгрупповые различия
+
+#тест на межгрупповые различия для диеты
 fit <- aov(weight.loss ~ diet.type, data=data)
 summary(fit)
+#тест на межгрупповые различия для гендера
+fit <- aov(weight.loss ~ gender, data=data)
+summary(fit)
+
 
 #попарные различия между средними значениями для всех групп
 TukeyHSD(fit)
@@ -52,26 +67,3 @@ library(multcomp)
 par(mar=c(5,4,6,2))
 tuk <- glht(fit, linfct=mcp(diet.type="Tukey"))
 plot(cld(tuk, level=.05),col="lightgrey")
-
-#Задание
-
-
-#Добавить проверку на выборы и избавиться от них
-#Добавляем проверку на зависисмость потери веса от пола 
-boxplot(weight.loss~gender,data=data,col="light gray",
-        ylab = "Weight loss (kg)", xlab = "Diet type")
-abline(h=0,col="green")
-#проверим сбалансированные ли данные
-table(data$diet.type)
-
-#График групповых средних
-plotmeans(weight.loss ~ gender, data=data)
-aggregate(data$weight.loss, by = list(data$gender), FUN=sd)
-
-#Для подгонки ANOVA модели используем функцию aov, частный случай линейной модели lm
-#тест на межгрупповые различия
-fit <- aov(weight.loss ~ gender, data=data)
-summary(fit)
-#попарные различия между средними значениями для всех групп
-TukeyHSD(fit)
-
